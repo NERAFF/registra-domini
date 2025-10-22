087d1d9ee05dc4f2241ec7dc5f2f44710aae0e4a
NERAFF <lucalau0405@gmail.com>
2025-10-22 23:20:39 +0200

    added filters for bypass cors block

---
+++ server-web/src/main/java/it/unimib/sd2024/filters/CorsFilter.java
@@ -0,0 +1,21 @@
+package it.unimib.sd2024.filters;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestCtx,
+                       ContainerResponseContext responseCtx) throws IOException {
+        responseCtx.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseCtx.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseCtx.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseCtx.getHeaders().add("Access-Control-Allow-Headers",
+            "Content-Type, Authorization, X-Requested-With");
+    }
+}
